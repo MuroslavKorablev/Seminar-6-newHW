247f946a3dc676076f186b058575d19f41a60ef8
Kleo <mmttdd16@gmail.com>
2023-09-28 16:51:16 +0300

    Finished Ex1

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел.
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+        int count = GetCountFromUser();
+        int positiveCount = CountPositiveNumbers(count);
+        PrintResult(positiveCount);
+
+
+     int GetCountFromUser()
+    {
+        Console.Write("Введите количество чисел, которые вы хотите ввести: ");
+        return int.Parse(Console.ReadLine()!);
+    }
+
+    int CountPositiveNumbers(int count)
+    {
+        int positiveCount = 0;
+
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write($"Введите число {i + 1}: ");
+            int number = int.Parse(Console.ReadLine()!);
+
+            if (number > 0)
+            {
+                positiveCount++;
+            }
+        }
+
+        return positiveCount;
+    }
+
+    static void PrintResult(int positiveCount)
+    {
+        Console.WriteLine($"Количество чисел больше 0: {positiveCount}");
+    }
